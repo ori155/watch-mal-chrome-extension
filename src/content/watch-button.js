ee6b79d56dd573ba0810209cca8841b50a9b18b5
ori <ori155@gmail.com>
2016-09-09 00:06:44 +0300

    Add click functionality
    button sends https request to gogoanime.io/ when clicked.

--- src/content/watch-button.js
+++ src/content/watch-button.js
@@ -2,13 +2,37 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import '../vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 
+
+let request_init = {
+	credentials: 'include',
+	cors: 'cors'
+	};
+let url = 'https://gogoanime.io/';
+
 export class WatchButton extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			available: false
+		};
+	}
+	handleClick = () => {
+		fetch(url, request_init)
+		.then(res => {
+			console.log(
+				'same-origin, cors! recived responce ' +
+				'from ' + res.url +
+				'\nres is ' + res.status);
+			});
 	}
 	render() {
 		return (
-		    <Button bsStyle="primary">bootstrap</Button>
+		    <Button
+		    bsStyle="success"
+		    disabled={this.state.available}
+		    onClick={this.handleClick}>
+		    	GoGoAnime
+		    </Button>
 		    );
 	}
 }
