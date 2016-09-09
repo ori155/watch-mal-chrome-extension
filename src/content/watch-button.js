446cb91ada43e1a78b0737600ff2fc4fcf718f94
ori <ori155@gmail.com>
2016-09-09 03:01:00 +0300

    Make button click open the first episode
    needs heavy refactor

--- src/content/watch-button.js
+++ src/content/watch-button.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import '../vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 
-
 let request_init = {
 	credentials: 'include',
 	cors: 'cors'
 	};
-let url = 'https://gogoanime.io/';
+let base_url = 'https://gogoanime.io/';
+let url = base_url + 'search.html?keyword=';
+
+function extract_anime_data(page_text) {
+	let reg = /<p class="name">[\s]*<a href="([^"]+)" title="([^"]+)"/;
+	let [_, url, title] = reg.exec(page_text);
+	let insite_name = /\/category\/([^/]+)/.exec(url)[1];
+	return {url, title, insite_name};
+}
+
+function get_anime_data(search_url) {
+	return fetch(search_url, request_init)
+		.then(res => res.text())
+		.then( text => extract_anime_data(text));
+}
+
 
 export class WatchButton extends React.Component {
 	constructor(props) {
@@ -17,12 +31,18 @@ export class WatchButton extends React.Component {
 		};
 	}
 	handleClick = () => {
-		fetch(url, request_init)
-		.then(res => {
-			console.log(
-				'same-origin, cors! recived responce ' +
-				'from ' + res.url +
-				'\nres is ' + res.status);
+		let title_node = document.getElementsByClassName("h1")[0];
+		let title = title_node.getElementsByTagName('span')[0]
+				      .innerText;
+		get_anime_data(url + encodeURIComponent(title))
+			.then(anime => {
+				console.log(anime.title + ' '
+					+ anime.url +
+					' insite: ' + anime.insite_name);
+				window.open(
+				base_url.replace('https', 'http') +
+				anime.insite_name +
+					'-episode-' + 1, '_blank');
 			});
 	}
 	render() {
--- webpack.babel.js
+++ webpack.babel.js
@@ -1,3 +1,13 @@
 module.exports = {
-	"presets": ["react"]
+	"presets": ["react"],
+	"plugins": [
+//		"transform-async-to-generator",
+		"syntax-async-functions",
+		["transform-regenerator", {
+		        asyncGenerators: false, // true by default
+		        generators: false, // true by default
+		        async: true // true by default
+		    }]
+
+		]
 }
