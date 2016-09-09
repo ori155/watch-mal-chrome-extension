197afce38767fa017715a98163f377d5a53366a6
ori <ori155@gmail.com>
2016-09-09 12:51:02 +0300

    Refactor the anime watch page fetching process
    create a Fetch class

--- src/content/watch-button.js
+++ src/content/watch-button.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+
 import '../vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 
-let request_init = {
-	credentials: 'include',
-	cors: 'cors'
-	};
-let base_url = 'https://gogoanime.io/';
-let url = base_url + 'search.html?keyword=';
-
-function extract_anime_data(page_text) {
-	let reg = /<p class="name">[\s]*<a href="([^"]+)" title="([^"]+)"/;
-	let [_, url, title] = reg.exec(page_text);
-	let insite_name = /\/category\/([^/]+)/.exec(url)[1];
-	return {url, title, insite_name};
-}
-
-function get_anime_data(search_url) {
-	return fetch(search_url, request_init)
-		.then(res => res.text())
-		.then( text => extract_anime_data(text));
-}
+import GoGoFetch from '../fetchers/gogoanime';
 
 
 export class WatchButton extends React.Component {
@@ -34,16 +17,7 @@ export class WatchButton extends React.Component {
 		let title_node = document.getElementsByClassName("h1")[0];
 		let title = title_node.getElementsByTagName('span')[0]
 				      .innerText;
-		get_anime_data(url + encodeURIComponent(title))
-			.then(anime => {
-				console.log(anime.title + ' '
-					+ anime.url +
-					' insite: ' + anime.insite_name);
-				window.open(
-				base_url.replace('https', 'http') +
-				anime.insite_name +
-					'-episode-' + 1, '_blank');
-			});
+		GoGoFetch.open_episode(title, 3);
 	}
 	render() {
 		return (
---
+++ src/fetchers/gogoanime.js
@@ -0,0 +1,40 @@
+
+let request_init = {
+	credentials: 'include',
+	cors: 'cors'
+	};
+
+const base_url = 'https://gogoanime.io/';
+const search_url = base_url + 'search.html?keyword=';
+
+export default class GoGoFetch {
+	static get_anime_data(name) {
+		return this.search_anime(name)
+			.then(res => res.text())
+			.then( text => this.analyze_search_page(text));
+	}
+	static analyze_search_page(page_text) {
+	let reg =
+		/<p class="name">[\s]*<a href="([^"]+)" title="([^"]+)"/;
+	let [_, url, title] = reg.exec(page_text);
+	let insite_name = /\/category\/([^/]+)/.exec(url)[1];
+	return {url, title, insite_name};
+	}
+	static search_anime(name) {
+		return fetch(search_url + name, request_init);
+	}
+	static episode_url(anime, ep) {
+		ep = +ep;
+		return base_url.replace(/^https/, 'http') +
+				anime.insite_name +
+				'-episode-' + ep;
+	}
+	static open_episode(name, ep) {
+		this.get_anime_data(name)
+			.then( anime => {
+				window.open(
+				this.episode_url(anime, ep), '_blank');
+			});
+	}
+
+}
